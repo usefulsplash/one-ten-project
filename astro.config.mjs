d44cd0dead72ec1111246f1a3cdeead659f59eb1
usefulsplash <emjavier16@gmail.com>
2023-11-18 15:25:35 -0500

    tailwind added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: vercel()
+  adapter: vercel(),
+  integrations: [tailwind()]
 });
